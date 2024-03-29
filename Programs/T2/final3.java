e1102afebf546a10c36d9cc0ffa807b7b57d3c62
Paavan Trivedi <2021002171210190@ljku.edu.in>
2024-03-29 19:43:03 +0530

    Create final3.java

---
+++ Programs/T2/final3.java
@@ -0,0 +1,17 @@
+final class bike {
+    bike() {
+        System.out.println("Default Bike Constructor");
+    }
+}
+
+class Honda extends bike {
+    Honda() {
+        System.out.println("Default Honda Constructor");
+    }
+}
+
+class Main23 {
+    public static void main(String[] args) {
+        Honda obj = new Honda();
+    }
+}
