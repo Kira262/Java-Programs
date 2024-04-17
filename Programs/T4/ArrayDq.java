7a57e295e2ee4506a3b160227aed1ae1b06d67f9
Paavan Trivedi <2021002171210190@ljku.edu.in>
2024-04-17 21:41:48 +0530

    a

---
+++ Programs/T4/ArrayDq.java
@@ -0,0 +1,9 @@
+import java.util.ArrayDeque;
+
+class maine {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> AD1 = new ArrayDeque<>();
+        ArrayDeque<Integer> AD2 = new ArrayDeque<>(10);
+        // ArrayDeque<Integer> AD3 = new ArrayDeque<>(Comparator.reverseOrder());
+    }
+}
